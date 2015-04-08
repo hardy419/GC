9f4ffb34aded5888a671d1c9b719fe40a574cef2
Hardy <540115739@qq.com>
2015-04-08 14:49:03 +0800

    Change project name --> gc; Mv container div to header/footer; Modified navigator, container width:900px, banner/head/footer width:1100px; Saparate index.html,index2.html,etc. (temp.). index3.html; Files added;
    Signed-off-by: Hardy <540115739@qq.com>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 if(version_compare(PHP_VERSION, '5.3.0','<'))die('require PHP > 5.3.0 !');
 
-define('SITE_PATH','http://localhost/vkids/');
+define('SITE_PATH','http://192.168.1.101/gc/');
 define('APP_PATH','./Application/');
 define('APP_DEBUG',true);
 require './ThinkPHP/ThinkPHP.php';
