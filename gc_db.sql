9aa45b15362ca3b44828fc6a699a1f408c94c91a
Hardy <540115739@qq.com>
2015-04-11 15:43:24 +0800

    Project admin

--- gc_db.sql
+++ gc_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2015 年 04 月 10 日 07:08
+-- 生成日期: 2015 年 04 月 11 日 05:53
 -- 服务器版本: 5.5.24-log
 -- PHP 版本: 5.3.13
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `gc_banner` (
   `url` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `sid` (`sid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
 
 --
 -- 转存表中的数据 `gc_banner`
@@ -58,6 +58,28 @@ INSERT INTO `gc_banner` (`id`, `title`, `sid`, `url`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `gc_catagory`
+--
+
+CREATE TABLE IF NOT EXISTS `gc_catagory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `type` enum('項目','備用1','備用2') CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `gc_catagory`
+--
+
+INSERT INTO `gc_catagory` (`id`, `name`, `type`) VALUES
+(1, '類別1', '項目'),
+(2, '類別2', '項目'),
+(3, '類別3', '項目');
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `gc_page`
 --
 
@@ -67,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `gc_page` (
   `content` text NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=90006 ;
 
 --
 -- 转存表中的数据 `gc_page`
@@ -82,6 +104,32 @@ INSERT INTO `gc_page` (`id`, `title`, `content`, `status`) VALUES
 (90003, 'What We Do', '    &lt;div class=&quot;whatwedo-intro&quot;&gt;\n        &lt;img src=&quot;../../../../Public/Img/GC/whatwedo_pic1.png&quot;&gt;&lt;/img&gt;\n        &lt;div class=&quot;intro-text&quot;&gt;\n            &lt;div class=&quot;intro-title&quot;&gt;Marketing and branding communications&lt;/div&gt;\n            &lt;p&gt;The keys to success in marketing are having strong brand awareness and a good reputation.  Effective marketing strategies are able to put brands and products under the spotlights and build consumer’s loyalty.  Having brands become sponsors of large- scale events is one of the strategies to enhance image of the brand.  GC helps clients identify the best opportunity to place sponsorships according to the nature of their business and market positioning.  With careful planning and the employment of promotion strategies, we pledged to provide the most cost-effective and special marketing campaign for clients.&lt;/p&gt;\n            &lt;div class=&quot;detail-title&quot;&gt;List of services provided by GC&lt;/div&gt;\n            &lt;div&gt;&lt;/div&gt;\n            &lt;ul&gt;\n                &lt;li&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w33&quot;&gt;Marketing communications&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Hosting press conferences&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Brand crossover and sponsorships&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w33&quot;&gt;Branding strategies&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Brand promotion and new product launch&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Production of promotion materials&lt;/li&gt;&lt;/ul&gt;\n                &lt;/li&gt;\n            &lt;/ul&gt;\n        &lt;/div&gt;\n        &lt;div style=&quot;clear:both;&quot;&gt;&lt;/div&gt;\n    &lt;/div&gt;\n    &lt;hr/&gt;\n    &lt;div class=&quot;whatwedo-intro&quot;&gt;\n        &lt;img src=&quot;../../../../Public/Img/GC/whatwedo_pic2.png&quot;&gt;&lt;/img&gt;\n        &lt;div class=&quot;intro-text&quot;&gt;\n            &lt;div class=&quot;intro-title&quot;&gt;Digital and social media marketing&lt;/div&gt;\n            &lt;p&gt;In today’s information age, making use of the internet, social media and mobile devices for marketing purposes is a growing trend.  GC has a team of digital marketing veterans who knows the online world inside out and are capable communicator and innovators in the digital world.  GC tailor-made social media promotion strategies for clients.  Through online forums, social media groups and bloggers, GC ensures client’s marketing messages reach the target audience in the most-cost-effective way, helping brands enhance their popularity.&lt;/p&gt;\n            &lt;p&gt;GC also monitors what is being said on social media about clients.  Through analysing these data, GC is able to come up with the best marketing strategies to help clients build and enhance market share.&lt;/p&gt;\n            &lt;div class=&quot;detail-title&quot;&gt;List of services provided by GC&lt;/div&gt;\n            &lt;div&gt;&lt;/div&gt;\n            &lt;ul&gt;\n                &lt;li&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Promotion on social media&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Blogger outreach&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Design and promotion strategies for product placement in online advertisement&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Social media and online advertising&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Planning for interactive online promotion activities&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Social media research and monitoring&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Corporate website design, website content development and updates&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Social media management&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Digital marketing strategies&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Gathering market updates and giving analysis&lt;/li&gt;&lt;/ul&gt;\n                &lt;/li&gt;\n            &lt;/ul&gt;\n        &lt;/div&gt;\n        &lt;div style=&quot;clear:both;&quot;&gt;&lt;/div&gt;\n    &lt;/div&gt;\n    &lt;hr/&gt;\n    &lt;div class=&quot;whatwedo-intro&quot;&gt;\n        &lt;img src=&quot;../../../../Public/Img/GC/whatwedo_pic3.png&quot;&gt;&lt;/img&gt;\n        &lt;div class=&quot;intro-text&quot;&gt;\n            &lt;div class=&quot;intro-title&quot;&gt;Media relations&lt;/div&gt;\n            &lt;p&gt;Giving media interviews and getting media coverage is the best way for brands and corporate to build reputation and boost brand awareness.  GC is familiar with the operation of traditional and online media.  It has close relationship with local media including newspaper, magazine, radio stations, television stations and news agencies, helping clients build trust among media and gain positive coverage.&lt;/p&gt;\n            &lt;div class=&quot;detail-title&quot;&gt;List of services provided by GC&lt;/div&gt;\n            &lt;div&gt;&lt;/div&gt;\n            &lt;ul&gt;\n                &lt;li&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w33&quot;&gt;Arranging media interviews&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Market positioning and indentifying news angles&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Hosting media gatherings&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w33&quot;&gt;Writing and distribution of press releases&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Organising of media tours&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Monitor media coverage&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w33&quot;&gt;Hosting press conferences&lt;/li&gt;&lt;li class=&quot;li-w33&quot;&gt;Hosting corporate events&lt;/li&gt;&lt;/ul&gt;\n                &lt;/li&gt;\n            &lt;/ul&gt;\n        &lt;/div&gt;\n        &lt;div style=&quot;clear:both;&quot;&gt;&lt;/div&gt;\n    &lt;/div&gt;\n    &lt;hr/&gt;\n    &lt;div class=&quot;whatwedo-intro&quot;&gt;\n        &lt;img src=&quot;../../../../Public/Img/GC/whatwedo_pic4.png&quot;&gt;&lt;/img&gt;\n        &lt;div class=&quot;intro-text&quot;&gt;\n            &lt;div class=&quot;intro-title&quot;&gt;Event planning&lt;/div&gt;\n            &lt;p&gt;Hosting activities, conferences, seminars and exhibitions are great ways for target consumers to learn about services and products.  Such events are also able to help brands create positive images and build relations with target customers.  GC helps clients host events from start to finish, providing one stop event planning for all kinds of activities.  From coming up with a theme to event planning, making the budget and locating venues, GC does it all.  GC provides clients with events that are able enhance the reputation of their brands.&lt;/p&gt;\n            &lt;p&gt;GC has a lot of experience in managing events.   It helps clients set-up venues, providing support such as on-stage lightings and sound systems and recruiting MCs and performers.  GC also invites celebrities to be spokesperson for brands or make appearances as guests for conferences and seminars.  GC brings clients unique and refreshing events.&lt;/p&gt;\n            &lt;div class=&quot;detail-title&quot;&gt;List of services provided by GC&lt;/div&gt;\n            &lt;div&gt;&lt;/div&gt;\n            &lt;ul&gt;\n                &lt;li&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Locating venues and providing design and production of venues&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Annual dinners&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Hosting activities and seminars&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Opening ceremonies&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Organise event run-downs&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Exhibitions and road shows&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Media reception and public relations&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Formal receptions&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Production of publications, souvenir and sourcing for supplies for events&lt;/li&gt;&lt;li class=&quot;li-w40&quot;&gt;Catering and transport arrangements for events&lt;/li&gt;&lt;/ul&gt;\n                    &lt;ul&gt;&lt;li class=&quot;li-w60&quot;&gt;Hosting seminars, forums, exhibitions and lectures&lt;/li&gt;&lt;/ul&gt;\n                &lt;/li&gt;\n            &lt;/ul&gt;\n        &lt;/div&gt;\n        &lt;div style=&quot;clear:both;&quot;&gt;&lt;/div&gt;\n    &lt;/div&gt;\n', 1),
 (90004, 'Client List', '&lt;div class=&quot;intro-title clientlist-title&quot;&gt;\r\n	Client List\r\n&lt;/div&gt;\r\n&lt;hr /&gt;\r\n&lt;div class=&quot;clientlist-div&quot;&gt;\r\n	&lt;ul&gt;\r\n		&lt;li&gt;\r\n			&lt;ul&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo1.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo2.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo3.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n			&lt;/ul&gt;\r\n			&lt;ul&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo4.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo5.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo6.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n			&lt;/ul&gt;\r\n			&lt;ul&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo7.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo8.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo9.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n			&lt;/ul&gt;\r\n			&lt;ul&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo10.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo11.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo12.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n			&lt;/ul&gt;\r\n			&lt;ul&gt;\r\n				&lt;li class=&quot;li-w33&quot;&gt;\r\n					&lt;a href=&quot;http://192.168.1.101/gc/admin.php/Pageadmin/add/id/#&quot;&gt;&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/client_logo13.png&quot; /&gt;&lt;/a&gt;\r\n				&lt;/li&gt;\r\n			&lt;/ul&gt;\r\n		&lt;/li&gt;\r\n	&lt;/ul&gt;\r\n	&lt;div style=&quot;clear:both;&quot;&gt;\r\n	&lt;/div&gt;\r\n&lt;/div&gt;', 1),
 (90005, 'Contact Us', '&lt;div id=&quot;contact-div&quot;&gt;\r\n	&lt;div class=&quot;intro-title&quot;&gt;\r\n		CONTACT US\r\n	&lt;/div&gt;\r\n	&lt;form name=&quot;contact&quot; action=&quot;#&quot; method=&quot;GET&quot;&gt;\r\n		&lt;table cellspacing=&quot;10&quot;&gt;\r\n			&lt;tbody&gt;\r\n				&lt;tr&gt;\r\n					&lt;td&gt;\r\n						Name:\r\n					&lt;/td&gt;\r\n					&lt;td&gt;\r\n						&lt;input name=&quot;name&quot; type=&quot;text&quot; /&gt;\r\n					&lt;/td&gt;\r\n				&lt;/tr&gt;\r\n				&lt;tr&gt;\r\n					&lt;td&gt;\r\n						Email:\r\n					&lt;/td&gt;\r\n					&lt;td&gt;\r\n						&lt;input name=&quot;email&quot; type=&quot;text&quot; /&gt;\r\n					&lt;/td&gt;\r\n				&lt;/tr&gt;\r\n				&lt;tr&gt;\r\n					&lt;td&gt;\r\n						Phone:\r\n					&lt;/td&gt;\r\n					&lt;td&gt;\r\n						&lt;input name=&quot;phone&quot; type=&quot;text&quot; /&gt;\r\n					&lt;/td&gt;\r\n				&lt;/tr&gt;\r\n				&lt;tr&gt;\r\n					&lt;td&gt;\r\n						Message:\r\n					&lt;/td&gt;\r\n					&lt;td&gt;\r\n						&lt;textarea name=&quot;message&quot; rows=&quot;5&quot; cols=&quot;30&quot;&gt;&lt;/textarea&gt;\r\n					&lt;/td&gt;\r\n				&lt;/tr&gt;\r\n			&lt;/tbody&gt;\r\n		&lt;/table&gt;\r\n	&lt;/form&gt;\r\n&lt;/div&gt;\r\n&lt;div id=&quot;info-div&quot;&gt;\r\n	&lt;table cellspacing=&quot;10&quot;&gt;\r\n		&lt;tbody&gt;\r\n			&lt;tr&gt;\r\n				&lt;td&gt;\r\n					Tel:\r\n				&lt;/td&gt;\r\n				&lt;td&gt;\r\n					2898 8283\r\n				&lt;/td&gt;\r\n			&lt;/tr&gt;\r\n			&lt;tr&gt;\r\n				&lt;td&gt;\r\n					Fax:\r\n				&lt;/td&gt;\r\n				&lt;td&gt;\r\n					3914 6999\r\n				&lt;/td&gt;\r\n			&lt;/tr&gt;\r\n			&lt;tr&gt;\r\n				&lt;td&gt;\r\n					Email:\r\n				&lt;/td&gt;\r\n				&lt;td&gt;\r\n					info@gccomhk.com\r\n				&lt;/td&gt;\r\n			&lt;/tr&gt;\r\n			&lt;tr&gt;\r\n				&lt;td&gt;\r\n					Address:\r\n				&lt;/td&gt;\r\n				&lt;td&gt;\r\n					Rm B10, 1/F, King Yip Factory Bldg, 59 King Yip St., Kwun Tong, Hong Kong\r\n				&lt;/td&gt;\r\n			&lt;/tr&gt;\r\n		&lt;/tbody&gt;\r\n	&lt;/table&gt;\r\n&lt;img src=&quot;http://192.168.1.101/gc/Public/Img/GC/contact_map.png&quot; /&gt; \r\n&lt;/div&gt;\r\n&lt;div style=&quot;clear:both;&quot;&gt;\r\n&lt;/div&gt;', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `gc_project`
+--
+
+CREATE TABLE IF NOT EXISTS `gc_project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `preview` varchar(256) NOT NULL,
+  `images` varchar(4096) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- 转存表中的数据 `gc_project`
+--
+
+INSERT INTO `gc_project` (`id`, `title`, `cid`, `description`, `preview`, `images`) VALUES
+(1, '工程1', 1, '工程1描述', 'Public/Img/GC/brand.jpg', 'Public/Img/GC/brand.jpg|Public/Img/GC/bulb.png|add'),
+(2, '工程2', 1, '工程2描述', 'Public/Img/GC/client_logo2.png', 'Public/Img/GC/client_logo1.png|Public/Img/GC/client_logo2.png|Public/Img/GC/client_logo3.png|Public/Img/GC/client_logo4.png|Public/Img/GC/client_logo5.png|Public/Img/GC/client_logo6.png'),
+(5, 'TEST2', 2, 'ahaha', 'Public/Img/GC/client_logo12.png', 'Public/Img/GC/client_logo12.png|Public/Img/GC/client_logo11.png|hahahahah'),
+(7, 'Project3', 3, 'catagory3test', 'Public/Img/GC/client_logo5.png', 'Public/Img/GC/client_logo5.png');
 
 -- --------------------------------------------------------
 
@@ -106,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `gc_user` (
 --
 
 INSERT INTO `gc_user` (`id`, `account`, `login_count`, `last_login_time`, `password`, `status`, `nickname`, `last_login_ip`) VALUES
-(1, 'admin', '142', '1428644135', 'b59c67bf196a4758191e42f76670ceba', 1, '', '127.0.0.1');
+(1, 'admin', '145', '1428730536', 'b59c67bf196a4758191e42f76670ceba', 1, '', '127.0.0.1');
 
 -- --------------------------------------------------------
 
