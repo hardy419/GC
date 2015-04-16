2d06eea7b5d2f343ba7936dd7738682e4657b6fd
Hardy <540115739@qq.com>
2015-04-16 10:42:54 +0800

    Language switch support.
    Signed-off-by: Hardy <540115739@qq.com>

--- Application/Home/Controller/PageviewController.class.php
+++ Application/Home/Controller/PageviewController.class.php
@@ -4,7 +4,8 @@ use Think\Controller;
 class PageviewController extends BaseController {
     public function index(){
         $id=I('get.id', 1);
-        $list=M('page')->where(array('id'=>$id))->find();
+        $lang=cookie('gc_lang');
+        $list=M('page')->where(array('id'=>(2 == $lang)?$id+4:$id))->find();
         $refurl = M('banner')->where(array('sid'=>$id))->getField('url');
         $this->assign('list',$list);
         $this->assign('refurl',$refurl);
