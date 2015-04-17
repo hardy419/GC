b7c741896f6b461639285e1400df60ceb6cb5316
Hardy <540115739@qq.com>
2015-04-17 13:51:33 +0800

    Contact Us page send message/google map; Remove PageviewController; Main page, News&Events --> Contact Us
    Signed-off-by: Hardy <540115739@qq.com>

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,6 +2,17 @@
 return array(
 	'AUTH_Home_TOKEN'=>'_^&javakdyadmin222_',
 	'LOGIN_Home_TIMEOUT'=>3600,
+    //邮件配置
+     'THINK_EMAIL' => array(
+         'SMTP_HOST'   => 'smtp.qq.com', //SMTP服务器
+         'SMTP_PORT'   => '25', //SMTP服务器端口
+         'SMTP_USER'   => '2757144278@qq.com', //SMTP服务器用户名
+         'SMTP_PASS'   => '1q2w3e4r', //SMTP服务器密码
+         'FROM_EMAIL'  => '2757144278@qq.com', //发件人EMAIL
+         'FROM_NAME'   => 'Hardy', //发件人名称
+         'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
+         'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
+     ),
 	//'配置项'=>'配置值'
 	'TMPL_PARSE_STRING'  =>array(
 		
--- Application/Home/Controller/ClientlistController.class.php
+++ Application/Home/Controller/ClientlistController.class.php
@@ -1,14 +1,14 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class PageviewController extends BaseController {
+class ClientlistController extends BaseController {
     public function index(){
-        $id=I('get.id', 1);
+        $id=2;
         $lang=cookie('gc_lang');
         $list=M('page')->where(array('id'=>(2 == $lang)?$id+4:$id))->find();
         $refurl = M('banner')->where(array('sid'=>$id))->getField('url');
         $this->assign('list',$list);
         $this->assign('refurl',$refurl);
-        $this->display();
+        $this->display('Pageview/index');
     }
 }
