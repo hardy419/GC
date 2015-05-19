fd0f7c8c4a502c7d22d4b518cb1058150091f34d
Hardy <540115739@qq.com>
2015-05-19 15:57:48 +0800

    Home page: Service section data retrieved from back-end; Project page: Preview clickable; Header/Footer: minor changes on contents.
    Signed-off-by: Hardy <540115739@qq.com>

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,6 +2,7 @@
 return array(
 	'AUTH_Home_TOKEN'=>'_^&javakdyadmin222_',
 	'LOGIN_Home_TIMEOUT'=>3600,
+    'URL_HTML_SUFFIX'=>'',
     //邮件配置
      'THINK_EMAIL' => array(
          'SMTP_HOST'   => 'smtp.qq.com', //SMTP服务器
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,6 +5,8 @@ class IndexController extends BaseController {
     public function index(){
         $bannerurls = M('banner')->where('sid >= 80001 AND sid < 90000')->getField('url',true);
         $this->assign('refurl', $bannerurls);
+        $service = M('project')->query('SELECT p.id AS pid, p.title, p.description, p.preview, c.name AS category FROM gc_project p LEFT JOIN gc_catagory c ON p.cid=c.id');
+        $this->assign('service', $service);
         $this->display();
     }
 }
