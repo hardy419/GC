5a994aea4cbaa314211c12296dad7f4ce0968180
Hardy <540115739@qq.com>
2015-06-15 16:51:54 +0800

    index.php/Whoweare --> about.php, etc.
    Signed-off-by: Hardy <540115739@qq.com>

---
+++ service.php
@@ -0,0 +1,13 @@
+<?php 
+if(version_compare(PHP_VERSION, '5.3.0','<'))die('require PHP > 5.3.0 !');
+
+$_GET['c'] = 'Whatwedo';
+
+define('SITE_PATH','/gc/');
+define('APP_PATH','./Application/');
+define('APP_DEBUG',true);
+define('BANNER_WIDTH',1100);
+define('BANNER_HEIGHT',240);
+define('BANNER_HEIGHT_MAIN',330);
+require './ThinkPHP/ThinkPHP.php';
+?>
